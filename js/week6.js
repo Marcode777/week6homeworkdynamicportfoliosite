0f6919c2465e32be836e5bf4a48cfd1007837e9a
Marcode777 <marc102202@hotmail.com>
2015-12-05 17:02:46 -0500

    added jquery effects

--- js/week6.js
+++ js/week6.js
@@ -1,3 +1,4 @@
-$(document).ready(function() {
-  $("item").slideUp(1000).delay(1000).slideDown
-});
+   $(document).ready(function() {
+        $("li").slideUp(1000).delay(1000).slideDown(1000);
+      });
+    
