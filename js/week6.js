b61563bc9f10a93f7beb1a468ed0bf815cb72d8a
Marcode777 <marc102202@hotmail.com>
2015-12-06 20:30:55 -0500

    added a position element and some slide functionality with jquery

--- js/week6.js
+++ js/week6.js
@@ -1,3 +1,4 @@
    $(document).ready(function() {
-        $("div").slideUp(1000).delay(1000).slideDown(1000);
+        $(".div1").slideUp(1000).delay(1000).slideDown(1000);
       });
+      // don't forget that in JavaScript, classes are referred to with a " . " and ID's are referred to with a " # ""
