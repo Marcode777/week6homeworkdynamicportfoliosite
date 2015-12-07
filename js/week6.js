b365050cf4b1af5349a75561dfd3ccf5d77aafa3
Marcode777 <marc102202@hotmail.com>
2015-12-07 18:12:57 -0500

    changed background color, added change div background color upon hover, added linked in and email on fixed position lower right hand corner

--- js/week6.js
+++ js/week6.js
@@ -2,3 +2,4 @@
         $(".div1").slideUp(1000).delay(1000).slideDown(1000);
       });
       // don't forget that in JavaScript, classes are referred to with a " . " and ID's are referred to with a " # ""
+   
