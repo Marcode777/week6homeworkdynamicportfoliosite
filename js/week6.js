ea66b20ddbeaf85eeb4993413d49a837f90c6412
Marcode777 <marc102202@hotmail.com>
2015-12-08 17:34:55 -0500

    tried my best trying to add github api...

--- js/week6.js
+++ js/week6.js
@@ -1,5 +1,17 @@
    $(document).ready(function() {
         $(".div1").slideUp(1000).delay(1000).slideDown(1000);
+          $.ajax({
+            url: 'https://api.github.com/users/marcode777/repos',
+            type: 'GET',
+            success: function(repos){
+               for(var i= 0; i< repos.length; i++){
+                 $(".repolist").append();
+               }
+              
+
+
+            }
+          })
       });
       // don't forget that in JavaScript, classes are referred to with a " . " and ID's are referred to with a " # ""
    
